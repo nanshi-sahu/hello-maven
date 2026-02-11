b4e18c5fb485f70b8641d7a91eefc86ff6d3ede2
Ubuntu <ubuntu@ip-172-31-45-137.ap-south-1.compute.internal>
2026-02-11 07:54:18 +0000

    changed the console message in App.java file

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,4 +1,4 @@
-package com.devops;
+	package com.devops;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Testing webhooks to see CI in action" );
     }
 }
